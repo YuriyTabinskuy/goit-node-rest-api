a4e4c24666241de00f990136d3d405511c85896e
YuriyTabinskuy <118268946+YuriyTabinskuy@users.noreply.github.com>
2024-02-19 11:09:02 +0200

    done hw5

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,7 +2,13 @@ import User from "../models/User.js";
 import Joi from "joi";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import fs from "fs/promises";
+import path from "path";
+import gravatar from "gravatar";
+import Jimp from "jimp";
 import "dotenv/config";
+
+const avatarPath = path.resolve("public", "avatars");
 
 const userSchema = Joi.object({
   email: Joi.string().required(),
@@ -32,9 +38,13 @@ const signup = async (req, res, next) => {
       throw error;
     }
     const hashPassword = await bcrypt.hash(req.body.password, 10);
-
+    const httpUrl = gravatar.url(req.body.email, {
+      protocol: "http",
+      s: "250",
+    });
     const newUser = await User.create({
       ...req.body,
+      avatarURL: httpUrl,
       password: hashPassword,
     });
     res.status(201).json({
@@ -80,6 +90,7 @@ const login = async (req, res, next) => {
       token,
       user: {
         email: user.email,
+        avatarURL: user.avatarURL,
         subscription: user.subscription,
       },
     });
@@ -103,7 +114,7 @@ const logout = async (req, res, next) => {
     const user = await User.findByIdAndUpdate(_id, { token: "" });
     if (!user) {
       const error = new Error("Not authorized");
-      error.status(401);
+      error.status = 401;
       throw error;
     }
     res.status(204).json();
@@ -135,10 +146,47 @@ const updateSubscription = async (req, res, next) => {
     );
     if (!user) {
       const error = new Error("Not authorized");
-      error.status(401);
+      error.status = 401;
       throw error;
     }
     res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      const error = new Error("File not exist");
+      error.status = 404;
+      throw error;
+    }
+    const { path: oldPath, filename } = req.file;
+    const image = await Jimp.read(oldPath);
+    image.resize(250, 250);
+    await image.writeAsync(oldPath);
+    const newPath = path.join(avatarPath, filename);
+    await fs.rename(oldPath, newPath);
+    const avatarURL = path.join("avatars", filename);
+    const { _id } = req.user;
+    const user = await User.findByIdAndUpdate(
+      _id,
+      {
+        avatarURL,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!user) {
+      const error = new Error("Not authorized");
+      error.status = 401;
+      throw error;
+    }
+    res.json({
+      avatarURL,
+    });
   } catch (error) {
     next(error);
   }
@@ -150,4 +198,5 @@ export default {
   getCurrent,
   logout,
   updateSubscription,
+  updateAvatar,
 };
